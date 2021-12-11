54e012655caa2898bd4ec1474786efe2023468a7
Chirdeep Tomar <chirdeep.tomar@gmail.com>
2021-12-11 18:53:03 +0000

    Swagger working

--- src/main/kotlin/com/learn/micronaut/Application.kt
+++ src/main/kotlin/com/learn/micronaut/Application.kt
@@ -1,22 +1,25 @@
 package com.learn.micronaut
 
-import io.micronaut.runtime.Micronaut.*
-import io.swagger.v3.oas.annotations.*
-import io.swagger.v3.oas.annotations.info.*
+import io.micronaut.runtime.Micronaut.build
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.info.License
 
 @OpenAPIDefinition(
-        info = Info(
-                title = "learn-micronaut",
-                version = "0.0"
-        )
+    info = Info(
+        title = "Learn Micronaut API",
+        version = "0.0",
+        description = "Learning Micronaut through building an API",
+        license = License(name = "Apache 2.0")
+    )
 )
 object Api {
 }
 
 fun main(args: Array<String>) {
     build()
-            .args(*args)
-            .packages("com.learn.micronaut")
-            .start()
+        .args(*args)
+        .packages("com.learn.micronaut")
+        .start()
 }
 
